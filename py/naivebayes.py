6d82bc30dbc44cf2bb7d808ed4a1dad1b2fb9151
buptcai <45426129+buptcai@users.noreply.github.com>
2018-12-02 23:05:23 +0800

    Add files via upload

---
+++ py/naivebayes.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class naivebayesclassifier(object):
+
+	#data:(m,n),label:(m,) list
+	 def __init__(self,train_data,train_label):
+	 	self.data = np.array(train_data)
+	 	self.label = np.array(train_label)
+
+	 def naivebayes_predict(self,predict_data):
+	 	category = np.unique(self.label)
+	 	N = category.size
+	 	final_prob = []
+	 	for i in range(N):
+
+	 		Dc = np.sum(self.label==category[i])
+	 		prior_prob = (Dc+1)/(self.label.size+N)
+	 		prob = prior_prob
+
+	 		#feature
+	 		for j in range(self.data.shape[1]):
+	 			feature = np.unique(self.data[:,j])
+	 			Ni = feature.size #Ni
+	 			condition_prob = (np.sum(feature==predict_data[j])+1)/(Dc+Ni)
+	 			prob = prior_prob*condition_prob
+
+	 		final_prob.append(prob)
