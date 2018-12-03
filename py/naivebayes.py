6906fb142d580a6292e62d22aae262a24407d9c0
buptcai <45426129+buptcai@users.noreply.github.com>
2018-12-03 10:58:26 +0800

    Add files via upload

--- py/naivebayes.py
+++ py/naivebayes.py
@@ -16,12 +16,18 @@ class naivebayesclassifier(object):
 	 		Dc = np.sum(self.label==category[i])
 	 		prior_prob = (Dc+1)/(self.label.size+N)
 	 		prob = prior_prob
-
 	 		#feature
 	 		for j in range(self.data.shape[1]):
 	 			feature = np.unique(self.data[:,j])
 	 			Ni = feature.size #Ni
-	 			condition_prob = (np.sum(feature==predict_data[j])+1)/(Dc+Ni)
-	 			prob = prior_prob*condition_prob
+	 			condition_prob = (np.sum(self.data[:,j]==predict_data[j])+1)/(Dc+Ni)
+	 			prob = prob*condition_prob
 
 	 		final_prob.append(prob)
+	 	return final_prob
+
+def main():
+	#waiting for validation
+
+if __name__ == '__main__':
+	main()
